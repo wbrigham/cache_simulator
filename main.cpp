6806f6d7ddb0f28d8e1b75c580c145b99fe81daa
BettyHosler <ehosler@nd.edu>
2021-04-21 14:19:19 -0400

    Main file

--- main.cpp
+++ main.cpp
@@ -1,1 +1,6 @@
-//this is the main
+//Microsoft Preternship Project Team:
+//Will Brigham wbrigham@nd.edu
+//Elizabeth Hosler ehosler@nd.edu
+//Zachary Chua zchua@nd.edu
+//
+//This is a cache simulator. It will take data from an input trace file passed on the command line. It will then siulate the data in the file....
