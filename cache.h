a1a4bbbd302bf9083907c4d7e882b9fbca7350ab
William Brigham <wbrigham@nd.edu>
2021-04-29 14:34:11 -0400

    header

--- cache.h
+++ cache.h
@@ -1,43 +1,58 @@
-// Will Brigham and Zach Chua and Betty Hosler
+i// Will Brigham and Zach Chua and Betty Hosler
 // cache simulator
 
 // methods will be labeled as integers 1 (LRU), 2 (LFRU), 3 (RR)
-class cache{
 
-	private:
-		// lru data
-		int *lru;
-
-		// lfu data
-		int *lfu;
-
-		// rr data
-		int *rr;
-
-		// cache size
-		int size;
-
-		// misses counter
-		int misses;
-
-		// total number of times accessed
-		int toal;
-
-	public:
-    	//default constructor
-    	cache();
-		void init(int, bool);
-
-    	// simulates cache access taking address and way of cache
-    	void access(unsigned int, int);
+class cache
+{
+public:
+	// constructor, sets replacement method and size
+	cache(int,bool);
 	
-		// reset cache
-		void reset();
+	// the default constructor can be used and then init can be called
+	// later to set the size/replacement method
+	cache();
+	void init(int,bool);
+	
+	// resets cache
+	void reset();
+	
+	// simulates a cache access, takes address and "way" of cache
+	void access(unsigned int,int);
+	
+	// returns current miss rate
+	double rate();
 
-		// miss rate
-		double rate();
+	~cache();
 
-    	~cache();
+private:
+	// array for valid bits
+	bool *valid;
+	
+	// holds cache tags
+	int *tag;
+	
+	// holds data for lru/fifo replacement
+	int *lru;
+	
+	// gets set to replacement method in constructor
+	bool replacement_method;
+
+	// size of cache
+	int size;
+	
+	// counts misses
+	int misses;
+	
+	// counts total accesses
+	int total;
+	
+	// finds a tag in a sub-block of cache, returns offset from base
+	int find_tag(int,int,int);
+	
+	// same as above but also updates fifo array to record access
+	int fifo_offset(int,int);
+	
+	// decrements all values in a subarray of lru
+	void decrement_all(int,int,int);
 };
-
-
