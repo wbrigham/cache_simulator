ef83fa4163ea13acd1f462bdc6d839c25086286b
William Brigham <wbrigham@nd.edu>
2021-05-04 15:01:13 -0400

    edit

--- inc/cache.h
+++ inc/cache.h
@@ -1,39 +1,56 @@
-c// Will Brigham and Zach Chua and Betty Hosler
-// cache simulator
+// header file
 
-// methods will be labeled as integers 1 (LRU), 2 (LFRU), 3 (RR)
-class cache{
+
+class block
+{
 public:
-	cache(int,bool);
+	// constructor, sets replacement method and size
+	block(int,bool);
 	
-	cache();
+	// the default constructor can be used and then init can be called
+	// later to set the size/replacement method
+	block();
 	void init(int,bool);
 	
+	// resets cache
 	void reset();
 	
+	// simulates a cache access, takes address and "way" of cache
 	void access(unsigned int,int);
 	
+	// returns current miss rate
 	double rate();
 
-	~cache();
+	~block();
 
 private:
+	// array for valid bits
 	bool *valid;
 	
+	// holds cache tags
 	int *tag;
 	
+	// holds data for lru/fifo replacement
 	int *lru;
 	
+	// gets set to replacement method in constructor
 	bool replacement_method;
 
+	// size of cache
 	int size;
 	
+	// counts misses
 	int misses;
 	
+	// counts total accesses
 	int total;
-
+	
+	// finds a tag in a sub-block of cache, returns offset from base
 	int find_tag(int,int,int);
 	
-	int lru_offset(int,int);
+	// same as above but also updates fifo array to record access
+	int fifo_offset(int,int);
 	
+	// decrements all values in a subarray of lru
+	void decrement_all(int,int,int);
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,53 +1,186 @@
-<<<<<<< HEAD
-// main
-=======
-//Microsoft Preternship Project Team:
-//Will Brigham wbrigham@nd.edu
-//Elizabeth Hosler ehosler@nd.edu
-//Zachary Chua zchua@nd.edu
-//
-//This is a cache simulator. It will take data from an input trace file passed on the command line. It will then siulate the data in the file....
->>>>>>>
-
-#define NUM_CACHES 8
-
-#include <math.h>
-#include <fstream.h>
-#include <stdio.h>
 #include "block.h"
 
-int main(int argc,char **argv)
+
+// this is the constructor. It takes the size of the cache and the
+// replacement policy(0=LRU,1=FIFO) and initializes the values.
+block::block(int s,bool replacement)
+{	
+	// set size and replacement method
+	size=s;
+	replacement_method=replacement;
+
+	// array for valid bits
+	valid=new bool[size];
+	
+	// array for tags
+	tag=new int[size];
+	
+	// array to keep track of LRU/FIFO
+	lru=new int[size];
+	
+	// set arrays to zero initially
+	reset();
+}
+
+// empty constructor, init has to be called if this is used. 
+block::block()
 {
-	if(argc != 2)
+
+}
+
+// this is the destructor, it frees up the memory dynamically allocated
+// in the arrays.
+block::~block()
+{
+	// quit if arrays haven't been allocated
+	if(!valid)
+		return;
+
+	// otherwise delete them
+	delete valid;
+	delete tag;
+	delete lru;
+}
+
+// this function is the same as the first constructor. It exists so that
+// arrays of blocks can be declared with different size/replacements.
+void block::init(int s,bool replacement)
+{
+	// set size/replacement
+	size=s;
+	replacement_method=replacement;
+
+	// set/initialize arrays
+	valid=new bool[size];
+	tag=new int[size];
+	lru=new int[size];
+	reset();
+}
+
+// this function sets all arrays used in the class to zero and sets
+// the counts to zero
+void block::reset()
+{
+	// set all data in the arrays to zero
+	for(int i=0;i<size;++i)
 	{
-		// print error and quit
-		cout << "Usage: Invalid File\n";
-		return 0;
+		valid[i]=0;
+		lru[i]=0;
+		tag[i]=0;
 	}
 
-	// the input file
-	ifstream infile(argv[1]);
+	// reset counters
+	misses=0;
+	total=0;
+}
 
-	// data from input file, read one at a time
-	int read;
-
-	// caches
-	block caches[NUM_CACHES];
-
-	// initialize the caches to have varying sizes and replacement policies
-	for(int i=0;i<NUM_CACHES;++i)
-
-	// while there is data to be read
-	while(infile >> read)
+// this function takes an address and the "way" of the cache(ie 2 for
+// 2 way) and simulates a cache access, incrementing "misses" if there
+// is a miss.
+void block::access(unsigned int addr,int way)
+{
+	// increment total access count
+	++total;
+	
+	// calculate index to place data
+	int index=(int)(addr/16)%(size/way) * way;
+	
+	// calculate tag
+	int target_tag=addr/size*16;
+	
+	// if the cache is hit then find the byte offset from the index
+	int target_index=find_tag(index,way,target_tag);
+	
+	// if there's a cache hit
+	if(valid[index] && target_index)
 	{
-		// put the data in each cache, and set the "way" assiciativity different
-		// for each
-		for(i=0;i<NUM_CACHES;++i)
+		// if LRU replacement is chosen
+		if(replacement_method==0)
+		{
+			// if the byte being accessed isn't already the most
+			// recently used
+			if(lru[index+target_index-1]!=way-1)
+			{
+				// decrement all values in the LRU array with values
+				// greater than the value of the hit byte
+				decrement_all(index,way,lru[index+target_index-1]);
+				
+				// set the byte to be the most recently used
+				lru[index+target_index-1]=way-1;
+			}
+		}
 	}
+	// otherwise it's a miss
+	else
+	{
+		// increment miss count
+		++misses;
 
+		// set to valid
+		valid[index]=1;
 
-	// close input file
-	infile.close();
+		// set the tag and update the replacement array
+		tag[index+fifo_offset(index,way)]=target_tag;
+	}
+}
+
+// thos function takes a base index inside the cache and the "way" of
+// the cache and the target tag to find and returns the offset from
+// the index+1 of the tag data if it's found
+int block::find_tag(int index,int way,int target)
+{
+	// for all data in the cache line
+	for(int i=0;i<way;++i)
+		// if tag is found
+		if(tag[index+i]==target)
+			// return it's offset from index
+			return 1+i;
+	
+	// otherwise return false, it's not found	
+	return 0;
+}
+
+// this function returns the miss rate of the cache
+double block::rate()
+{
+	// misses/total = miss rate
+	return (double)misses/(double)total*100;
+}
+
+// this function takes a base index within the cache and the "way"
+// associativity and returns the offset from the index of the
+// value to replace. The replacement array "LRU" is also updated 
+int block::fifo_offset(int base,int way)
+{
+	// for all entries in the cache line
+	for(int i=0;i<way;++i)
+	{
+		// if we run into a byte with LRU=0
+		if(lru[i+base]==0)
+		{
+			// then mark that byte for replacement
+			// set that byte as the most recently used
+			lru[i+base]=way;
+
+			// then decrement all values to reflect the add
+			decrement_all(base,way,0);
+
+			// return offset from index of byte to replace
+			return i;				
+		}
+	}
 
 	return 0;
+}
+
+// this function takes a base value within the cache and the "way"
+// associativity and a value and decrements all values in the
+// replacement array that are greater than the value
+void block::decrement_all(int base,int way,int value)
+{
+	// for each byte in the subcache
+	for(int j=0;j<way;++j)
+		// decrement replacement value if > passed value
+		if(lru[j+base] > value)
+			--lru[j+base];
 }
