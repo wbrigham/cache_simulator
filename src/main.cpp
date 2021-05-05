834edac8a1066db2e835ffda02ee835b26fe03d7
William Brigham <wbrigham@nd.edu>
2021-05-05 15:33:52 -0400

    EDITS

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@
 // number of caches to use for debugging, set lower to get a quick test result
 
 
-#define NUM_CACHES 40
+#define CACHE_SIZE 8
 
 #include <math.h>
 #include <fstream.h>
@@ -19,19 +19,13 @@
 
 int main(int argc,char **argv)
 {
-	// if fine name isn't passed in
+	
 	if(argc != 2)
 	{
 		// print error and quit
 		cout << "Usage: as3 input_file\n";
 		return 0;
 	}
-	
-	cout << "Execution will take about 20 seconds, please wait...\n";
-
-
-	// for loop counter
-	int i;
 
 	// the input file
 	ifstream infile(argv[1]);
@@ -40,21 +34,38 @@ int main(int argc,char **argv)
 	int read;
 
 	// caches
-	block caches[NUM_CACHES];	
-	
-	// initialize the caches to have varying sizes and replacement policies
-	for(i=0;i<NUM_CACHES;++i)
-		caches[i].init(64*(int)pow(2,i%5),(i>19));
+	block caches;
 
 	// while there is data to be read
+	int i = 0;
+
+	int replacement;
+
 	while(infile >> read)
 	{
 		// put the data in each cache, and set the "way" assiciativity different
 		// for each
-		for(i=0;i<NUM_CACHES;++i)
-			caches[i].access(read,(int)pow(2,(i/5))%16+(int)pow(2,(i/5))/16);
+		caches.access(read, replacement  = 0);
+		i++;
+	}
+
+	int i = 0;
+	while(infile >> read)
+	{
+		// put the data in each cache, and set the "way" assiciativity different
+		// for each
+		caches.access(read, replacement  = 1);
+		i++;
 	}
 	
+	int i = 0;
+	while(infile >> read)
+	{
+		// put the data in each cache, and set the "way" assiciativity different
+		// for each
+		caches.access(read, replacement  = 2);
+		i++;
+	}
 	// print title nicely
 	printf("%25s\n\n%14d%10d%10d%10d%11d\n","LRU",1024,2048,4096,8192,16384);
 	
