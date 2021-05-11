434f73b5413a3a059ac72667ea574344dec7b1b9
William Brigham <wbrigham@nd.edu>
2021-05-10 20:25:05 -0400

    lru

--- src/lru.cpp
+++ src/lru.cpp
@@ -1,13 +1,45 @@
 #include <iostream>
 #include <stdlib.h>
+#include <bits/stdc++.h>
 using namespace std;
 struct Node {
-	int data;
+	long unsigned int data;
 	struct Node *prev;
 	struct Node *next;
 };
 struct Node* head = NULL;
 int counter = 0;
+int hit = 0;
+int miss = 0;
+int track = 0;
+
+void deleteHit(Node *head, int n){
+	Node* ptr;
+	ptr = head;
+	for(int i = 0; i < n; i++){
+		ptr = head->next;
+		i++;
+	}
+	Node* del = ptr;
+	Node* prev = head;
+    while(prev->next != NULL && prev->next != del) 
+        prev = prev->next; 
+  
+    // Check if node really exists in Linked List 
+    if(prev->next == NULL) 
+    { 
+        cout << "\nGiven node is not present in Linked List"; 
+        return; 
+    } 
+  
+    // Remove node from Linked List 
+    prev->next = prev->next->next; 
+  
+    // Free memory 
+    free(del); 
+  
+    return; 
+} 	
 void deleteNode() {
       if(head != NULL) {
         if(head->next == NULL) {
@@ -22,10 +54,18 @@ void deleteNode() {
         }
       }
 }
-void insert(int newdata, const char* read) {
+void insert(long unsigned int newdata, const char* read) {
 	if(read == "W"){
 		if(counter == 8){
 			deleteNode();
+			struct Node* newnode = (struct Node*)malloc(sizeof(struct Node));
+			newnode->data = newdata;
+			newnode->prev = NULL;
+			newnode->next = head;
+			if(head != NULL){
+				head->prev = newnode ;
+			}
+			head = newnode;
 		} 		
 		else{
 			struct Node* newnode = (struct Node*)malloc(sizeof(struct Node));
@@ -39,7 +79,27 @@ void insert(int newdata, const char* read) {
 			counter++;
 		}
 	}
+	else if(read == "R"){
+		struct Node* ptr;
+		ptr = head;
+		while(ptr != NULL){
+			if(newdata == ptr->data){
+				hit++;
+				deleteHit(head, track);
+				insert(newdata, "W");
+				break;		
+			}
+			else{
+				ptr = ptr->next;
+			}
+			track++;
+		}
+		if(ptr == NULL){
+			miss++;
+		}
+	}
 }
+  
 void display() {
 	struct Node* ptr;
 	ptr = head;
@@ -49,8 +109,11 @@ void display() {
     }
 }
 int main() {
+	insert(4, "W");
 	insert(3, "W");
-	insert(1, "W");
+	insert(7, "W");
+	insert(3, "R");
+	/*insert(1, "W");
 	insert(7, "W");
 	insert(2, "W");
 	insert(9, "W");
@@ -59,6 +122,7 @@ int main() {
 	insert(7, "W");
 	insert(2, "W");
 	insert(3, "W");
+	insert(3,"R");*/
 	cout<<"The doubly linked list is: \n";
 	display();
 	cout<< "\n" ;
